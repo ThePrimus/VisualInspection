96a3d15f4ce2fb1aba177470736cae754bbc4848
ThePrimus <patrick.petersen91@googlemail.com>
2016-05-30 15:17:54 +0200

    Added opencv test

---
+++ VisualInspection/Test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "opencv2/opencv.hpp"
+using namespace cv;
+
+int main() {
+	//short camera test in opencv
+	Mat img;
+
+
+	VideoCapture cap;
+	cap.open(0);
+	VideoCapture cp;
+
+	namedWindow("window", 1);
+	while (1) {
+		cap >> img;
+		imshow("window", img);
+		waitKey(33);
+	}
+}
