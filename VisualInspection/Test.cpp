89013e53656da8ef8f8746995d57245466898df0
ftbmynameis <ftbmynameis@gmail.com>
2016-06-09 15:44:38 +0200

    Cleanup main, add folders for images and renderings.

--- VisualInspection/Test.cpp
+++ VisualInspection/Test.cpp
@@ -1,19 +1,15 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
-#include "opencv2/opencv.hpp"
+#include <map>
+#include <functional>
+
+#include "detect_quad.h"
+
 using namespace cv;
+using namespace std;
 
-int main() {
-	//short camera test in opencv
-	Mat img;
-
-
-	VideoCapture cap;
-	cap.open(0);
-
-	namedWindow("window", 1);
-	while (1) {
-		cap >> img;
-		imshow("window", img);
-		waitKey(33);
-	}
+int main(int argc, char* argv[]) {
+	return 0;
 }
