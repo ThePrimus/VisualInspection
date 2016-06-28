4f153e10f2a2e308252dcda1fdefcc534362297b
ftbmynameis <ftbmynameis@gmail.com>
2016-06-28 08:59:21 +0200

    Cleanup and demo fixing.

--- VisualInspection/Test.cpp
+++ VisualInspection/Test.cpp
@@ -11,24 +11,27 @@
 using namespace std;
 using namespace cv;
 
-
-
-using namespace std;
+char* window_name = "Visual Inspection";
 
 int main(int argc, char* argv[]) {
 
-	Mat workpiece, canny, edges, removed_circles, outline;
-	char* window_name = "Damage Detection";
+	Mat workpiece;
 	bool error = false;
 	RotatedRect rect;
 	CircleDetection cd;
 
 	//quad detection
+	cout << "Quad detection" << endl;
 	loadImage(&workpiece);
 	detect_quad(workpiece, 0.0125, 0.2, 2, 50, 8, &rect);
+	draw_quad_info(workpiece, &rect, Scalar(0, 255, 0), NULL, Scalar(0, 0, 0));
 
+	namedWindow(window_name, WINDOW_NORMAL);
+	imshow(window_name, workpiece);
+	waitKey(0);
 
 	//circle detection
+	cout << "Circle detection" << endl;
 	loadImage(&workpiece);
 	cd.setRotatedRect(rect);
 	cd.setImage(workpiece);
@@ -37,20 +40,18 @@ int main(int argc, char* argv[]) {
 	std::vector<cv::Vec3f> circles = cd.getCircles();
 	cv::Mat imgWithDrawnCircles = cd.drawCircles();
 
-	namedWindow("Circles", WINDOW_NORMAL);
-	imshow("Circles", imgWithDrawnCircles);
-	
+	namedWindow(window_name, WINDOW_NORMAL);
+	imshow(window_name, imgWithDrawnCircles);
+	waitKey(0);
 
 	//damage detection
+	cout << "Damage detection" << endl;
 	loadImage(&workpiece);
 	error = detect_damage(&workpiece, &rect, circles, 25, 8, 2, 3);
 
 	namedWindow(window_name, WINDOW_NORMAL);
 	imshow(window_name, workpiece);
-
-
 	waitKey(0);
-	return 0;
 	
 	return 0;
 }
