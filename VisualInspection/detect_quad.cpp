41b808a596b0e5c2cb52a0d88c66a69359144dc0
ftbmynameis <ftbmynameis@gmail.com>
2016-07-11 15:22:14 +0200

    fixedyfix

--- VisualInspection/detect_quad.cpp
+++ VisualInspection/detect_quad.cpp
@@ -10,65 +10,7 @@ using namespace cv;
 using namespace std;
 
 extern double PX2CM;
-
-auto rotatedrect_area(RotatedRect rect) {
-	return rect.size.area();
-}
-
-bool check_quad_size(const RotatedRect& rect, const double px2cm, const double thresh_accuracy, const double side_length) {
-	Point2f rect_points[4];
-	rect.points(rect_points);
-
-	for (int i = 0; i < 4; i++) {
-		auto diff_p = rect_points[i] - rect_points[(i + 1) % 4];
-		auto length_px = sqrt(diff_p.x * diff_p.x + diff_p.y * diff_p.y);
-		auto length_cm = length_px * px2cm;
-		auto abs_length_diff = abs(length_cm - side_length);
-		if (abs_length_diff > thresh_accuracy) {
-			return false;
-		}
-	}
-	return true;
-}
-
-bool detect_quad(Mat in, double thresh_accuracy, double alpha, double beta, double side_length, RotatedRect* out_rect = NULL, vector<Point>* out_cont = NULL) {
-	using Pvec = vector<Point>;
-	Mat canny_output;
-	vector<Pvec> contours;
-	vector<Vec4i> hierarchy;
-
-	Mat img;
-	//cv::cvtColor(in, img, CV_BGR2GRAY);
-
-	in.convertTo(img, -1, alpha, beta);
-
-	/// Detect edges using canny
-	auto tresh = 100;
-	Canny(img, canny_output, tresh, tresh * 2, 3);
-	/// Find contours
-	findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-	std::sort(contours.begin(), contours.end(), [](const Pvec& a, const Pvec& b) { return minAreaRect(a).size.area() > minAreaRect(b).size.area(); });
-
-	if (contours.size() > 0) {
-		auto cont = contours[0];
-		auto rect = minAreaRect(cont);
-
-		if (out_rect)
-			*out_rect = minAreaRect(cont);
-		if (out_cont)
-			*out_cont = cont;
-
-		if (check_quad_size(rect, PX2CM, thresh_accuracy, side_length)) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	else {
-		return false;
-	}
-}
+extern char* window_name;
 
 void draw_rotated_rect(Mat out, const RotatedRect& rect, Scalar color) {
 	Point2f points[4];
@@ -90,4 +32,118 @@ void draw_quad_info(Mat out, RotatedRect* rect, Scalar rect_color, vector<Point>
 	if (cont)
 		draw_contour(out, *cont, cont_color);
 
+}
+
+auto rotatedrect_area(RotatedRect rect) {
+	return rect.size.area();
+}
+
+bool check_quad_size(const RotatedRect& rect, const double px2cm, const double thresh_accuracy, const double side_length) {
+	Point2f rect_points[4];
+	rect.points(rect_points);
+
+	for (int i = 0; i < 4; i++) {
+		auto diff_p = rect_points[i] - rect_points[(i + 1) % 4];
+		auto length_px = sqrt(diff_p.x * diff_p.x + diff_p.y * diff_p.y);
+		auto length_cm = length_px * px2cm;
+		auto abs_length_diff = abs(length_cm - side_length);
+		if (abs_length_diff > thresh_accuracy) {
+			return false;
+		}
+	}
+	return true;
+}
+
+Mat image_preprocessing(Mat img) {
+	Mat result;
+
+	//in.convertTo(img, -1, 1, 0);
+	cv::threshold(img, result, 170, 0, THRESH_TOZERO_INV);
+	//cv::namedWindow("test", WINDOW_NORMAL);
+	//cv::imshow("test", result);
+	//waitKey(0);
+
+	cv::threshold(result, result, 60, 255, THRESH_BINARY);
+	//cv::imshow("test", result);
+	//waitKey(0);
+
+	int erosion_size = 3;
+	Mat element = getStructuringElement(MORPH_RECT,
+		Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+		Point(erosion_size, erosion_size));
+
+	/// Apply the erosion operation
+	erode(result, result, element);
+	//imshow(window_name, result);
+	//waitKey(0);
+
+
+
+	//blur(img, img, Size(3, 3));
+	//cv::imshow("test", img);
+	//waitKey(0);
+	
+	int dilation_size = 2;
+	element = getStructuringElement(MORPH_RECT,
+	Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+	Point(dilation_size, dilation_size));
+	/// Apply the dilation operation
+	dilate(result, result, element);
+	//cv::imshow(window_name, result);
+	//waitKey(0);
+	
+
+	/// Detect edges using canny
+	auto tresh = 100;
+	Mat canny_result;
+	Canny(result, canny_result, tresh, tresh * 2, 3);
+	
+	//cv::imshow(window_name, result);
+	//waitKey(0);
+
+	return canny_result;
+}
+
+bool detect_quad(Mat in, double thresh_accuracy, double alpha, double beta, double side_length, RotatedRect* out_rect = NULL, vector<Point>* out_cont = NULL) {
+	using Pvec = vector<Point>;
+	Mat canny_output;
+	vector<Pvec> contours;
+	vector<Vec4i> hierarchy;
+
+	Mat img = image_preprocessing(in);
+	//cv::cvtColor(in, img, CV_BGR2GRAY);
+
+	
+
+	
+
+	canny_output = img;
+
+	/// Find contours
+	findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+	std::sort(contours.begin(), contours.end(), [](const Pvec& a, const Pvec& b) { return minAreaRect(a).size.area() > minAreaRect(b).size.area(); });
+
+	if (contours.size() > 0) {
+		auto cont = contours[0];
+		auto rect = minAreaRect(cont);
+
+		if (out_rect)
+			*out_rect = minAreaRect(cont);
+		if (out_cont)
+			*out_cont = cont;
+
+		draw_quad_info(in, &rect, Scalar(255, 0, 0), NULL, Scalar(255, 0, 0));
+		cv::imshow(window_name, in);
+		waitKey(0);
+
+		if (check_quad_size(rect, PX2CM, thresh_accuracy, side_length)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	else {
+		return false;
+	}
 }
--- VisualInspection/detect_quad.h
+++ VisualInspection/detect_quad.h
@@ -9,11 +9,11 @@
 using namespace cv;
 using namespace std;
 
-auto rotatedrect_area(RotatedRect rect);
-bool check_quad_size(const RotatedRect& rect, const double px2cm, const double thresh_accuracy, const double side_length);
-bool detect_quad(Mat in, double thresh_accuracy, double alpha, double beta, double side_length, RotatedRect* out_rect = NULL, vector<Point>* out_cont = NULL);
-
-
 void draw_rotated_rect(Mat out, const RotatedRect& rect, Scalar color);
 void draw_contour(Mat out, const vector<Point>& contour, Scalar color);
 void draw_quad_info(Mat out, RotatedRect* rect, Scalar rect_color, vector<Point>* cont, Scalar cont_color);
+
+auto rotatedrect_area(RotatedRect rect);
+bool check_quad_size(const RotatedRect& rect, const double px2cm, const double thresh_accuracy, const double side_length);
+Mat image_preprocessing(Mat input);
+bool detect_quad(Mat in, double thresh_accuracy, double alpha, double beta, double side_length, RotatedRect* out_rect = NULL, vector<Point>* out_cont = NULL);
