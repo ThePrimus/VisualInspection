5fc103feba618c772d01c1422e6cdb951d8ebbd6
darcus <m.conzelmann@hotmail.de>
2016-07-14 03:28:53 +0200

    GUI

--- VisualInspection/Test.cpp
+++ VisualInspection/Test.cpp
@@ -25,7 +25,7 @@ using namespace cv;
 
 char* window_name = "Visual Inspection";
 
-bool IS_CALIBRATED = true;
+bool IS_CALIBRATED = false;
 double PX2CM = 0.00664624;
 
 const bool test_from_filepath = false;
@@ -34,7 +34,7 @@ const bool test_test_routine = false;
 //const string filepath = "Images/Neue Beleuchtung/Perfekt2.png";
 //const string filepath = "Images/Neue Beleuchtung/circles_test.png";
 const string filepath = "Renderings/Perfekt3.png";
-const string calibration_image = "Images/Neue Beleuchtung/Kalibrierung1.png"; 
+const string calibration_image = "Images/Neue Beleuchtung/Kalibrierung1.png";
 const double calibration_value = 2.0;
 const string IMAGE_FOLDER = "./Images/Neue Beleuchtung/resized";
 
@@ -44,19 +44,80 @@ double calibrate_px2cm(Mat in, double calibration_length = 1.0);
 
 vector<string> get_all_files_names_within_folder(string folder);
 void test_routine(string image_folder, string output_path = "tests.txt");
+void callBackFunc(int event, int x, int y, int flags, void* userdata);
 
+//GUI stuff
+Mat3b gui;
+Rect calibrate_button;
+Rect test_button;
+Rect video;
+Rect exit_button;
+Rect visual_result;
+
+Size size_image;
+Mat image_to_test;
+Mat resized_image;
+
+Mat test_button_image;
+Mat test_button_inactive_image;
+Mat calibrate_button_image;
+Mat exit_button_image;
+Mat new_image_button_image;
+Mat x_symbol;
+Mat check_mark;
+
+const string test_button_path = "GUI/test_button.png";
+const string test_button_inactive_path = "GUI/test_button_inactive.png";
+const string calibrate_button_path = "GUI/calibrate_button.png";
+const string exit_button_path = "GUI/exit_button.png";
+const string new_image_button_path = "GUI/new_image_button.png";
+const string x_symbol_path = "GUI/x_symbol.png";
+const string check_mark_path = "GUI/haekchen_symbol.png";
+
+bool exit_program = false;
+bool test_finished = false;
+bool overall_result = false;
 
 
 
 int main(int argc, char* argv[]) {
+
+	//GUI
+	gui = Mat3b(600, 1005, Vec3b(250, 250, 250));
+	calibrate_button = Rect(10, 10, 200, 40);
+	test_button = Rect(10, 55, 200, 40);
+	exit_button = Rect(10, 550, 200, 40);
+	video = Rect(220, 10, 772, 579);
+	visual_result = Rect(10, 345, 200, 200);
+
+	size_image = Size(772, 579);
+
+	test_button_image = imread(test_button_path);
+	test_button_inactive_image = imread(test_button_inactive_path);
+	calibrate_button_image = imread(calibrate_button_path);
+	new_image_button_image = imread(new_image_button_path);
+	x_symbol = imread(x_symbol_path);
+	check_mark = imread(check_mark_path);
+
+	exit_button_image = imread(exit_button_path);
+
+	test_button_inactive_image.copyTo(gui(test_button));
+	calibrate_button_image.copyTo(gui(calibrate_button));
+	exit_button_image.copyTo(gui(exit_button));
+	gui(visual_result) = Vec3b(250, 250, 250);
+
+	namedWindow(window_name, WINDOW_FULLSCREEN);
+	resizeWindow(window_name, 1024, 765);
+	setMouseCallback(window_name, callBackFunc);
+	//
+
 
 	if (test_test_routine) {
 		test_routine(IMAGE_FOLDER);
 		return 0;
 	}
 
-	namedWindow(window_name, WINDOW_NORMAL);
-	resizeWindow(window_name, 1024, 765);
+
 
 	HIDS hCam = 1; // 0
 	char* imgMem;
@@ -66,31 +127,28 @@ int main(int argc, char* argv[]) {
 	if (!test_from_filepath && (is_InitCamera(&hCam, NULL) != IS_SUCCESS)) {
 		return 0;
 	}
-	if(!test_from_filepath)
+	if (!test_from_filepath)
 	{
-	is_AllocImageMem(hCam, img_width, img_height, img_bpp, &imgMem, &memId);
-	is_SetImageMem(hCam, imgMem, memId);
-	is_SetDisplayMode(hCam, IS_SET_DM_DIB);
-	is_SetColorMode(hCam, IS_CM_RGB8_PACKED);
-	is_SetImageSize(hCam, img_width, img_height);
-	double enable = 1;
-	double disable = 0;
-	is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_GAIN, &disable, 0);
-	is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_WHITEBALANCE, &disable, 0);
-	is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_FRAMERATE, &disable, 0);
-	is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SHUTTER, &disable, 0);
-	is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SENSOR_GAIN, &disable, 0);
-	is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SENSOR_WHITEBALANCE, &disable, 0);
-	is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SENSOR_SHUTTER, &disable, 0);
-	is_SetGamma(hCam, 150);
+		is_AllocImageMem(hCam, img_width, img_height, img_bpp, &imgMem, &memId);
+		is_SetImageMem(hCam, imgMem, memId);
+		is_SetDisplayMode(hCam, IS_SET_DM_DIB);
+		is_SetColorMode(hCam, IS_CM_RGB8_PACKED);
+		is_SetImageSize(hCam, img_width, img_height);
+		double enable = 1;
+		double disable = 0;
+		is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_GAIN, &disable, 0);
+		is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_WHITEBALANCE, &disable, 0);
+		is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_FRAMERATE, &disable, 0);
+		is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SHUTTER, &disable, 0);
+		is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SENSOR_GAIN, &disable, 0);
+		is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SENSOR_WHITEBALANCE, &disable, 0);
+		is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SENSOR_SHUTTER, &disable, 0);
+		is_SetGamma(hCam, 150);
 	}
 	Mat source_image = imread(filepath);
 
-	cout << "Controls: <c> to calibrate the system" << endl << "<t> to test the image displayed in the window" << endl << "<e> to exit" << endl;
-	cout << "After testing / calibration is done, press any key to read the next image (only works for webcam build)" << endl;
 
-	while (true) {
-		Mat image_to_test;
+	while (!exit_program) {
 		if (test_from_filepath) {
 
 			image_to_test = imread(filepath);
@@ -100,54 +158,49 @@ int main(int argc, char* argv[]) {
 				break;
 			}
 
-		} else if (is_FreezeVideo(hCam, IS_WAIT) == IS_SUCCESS) {
+		}
+		else if (is_FreezeVideo(hCam, IS_WAIT) == IS_SUCCESS) {
 			void *pMemVoid; //pointer to where the image is stored
 			is_GetImageMem(hCam, &pMemVoid);
 			cv::Mat img(img_height, img_width, CV_8UC3, pMemVoid);
 			cv::cvtColor(img, image_to_test, CV_RGB2GRAY);
 		}
 
-		if (image_to_test.rows > 0) {
-			imshow(window_name, image_to_test);
+
+
+		if (!test_finished) {
+			resize(image_to_test, resized_image, size_image);
+			cvtColor(resized_image, resized_image, CV_GRAY2BGR);
+			resized_image.copyTo(gui(video));
+			gui(visual_result) = Vec3b(250, 250, 250);
 		}
-		int key = waitKey(10);
-
-		if (key == 'e')
-			break;
-
-		if (key == 's')
-			continue;
-
-		if (key == 'c') {
-			// set calibration image
-			if (test_from_filepath)
-				image_to_test = imread(calibration_image);
-
-			PX2CM = calibrate_px2cm(image_to_test, 2.0);
-			if (PX2CM > 0) {
-				cout << "==========" << endl;
-				cout << "System calibrated with value: " << PX2CM << endl;
-				IS_CALIBRATED = true;
+		else {
+			if (overall_result) {
+				check_mark.copyTo(gui(visual_result));
 			}
 			else {
-				cout << "==========" << endl;
-				cout << "Warning: calibration failed!" << endl;
+				x_symbol.copyTo(gui(visual_result));
 			}
 		}
-		if (key == 't') {
-			if (!IS_CALIBRATED) {
-				cout << "==========" << endl;
-				cout << "Warning: System not calibrated!" << endl;
-				continue;
-			}
-			test_image(image_to_test);
-		}
-		
-	}
 
-	cout << "==========" << endl;
-	cout << "PRESS ANY KEY IN CONSOLE TO SHUTDOWN THE PROGRAM!" << endl;
-	cin.get();
+
+		if (IS_CALIBRATED) {
+			test_button_image.copyTo(gui(test_button));
+			if (test_finished) {
+				new_image_button_image.copyTo(gui(test_button));
+			}
+			else {
+				test_button_image.copyTo(gui(test_button));
+			}
+		}
+		else {
+			test_button_inactive_image.copyTo(gui(test_button));
+		}
+
+		imshow(window_name, gui);
+
+		waitKey(10);
+	}
 
 	return 0;
 }
@@ -163,67 +216,58 @@ void test_image(Mat img, bool show) {
 
 	bool check_everything = false;
 
-	cout << "==========" << endl;
 	//quad detection
 	Mat workpiece = img;
 	Mat output_damage;
 	result_quad_detect = detect_quad(workpiece, 0.25, 2, 50, 8, &rect, &contour);
-	cout << "Quad detection: " << (result_quad_detect ? "OK" : "FAILED") << endl;
 
 	if (result_quad_detect || check_everything) {
 
 
 		//circle detection
 		double conversion = 0.1 / PX2CM; // 1mm in pixel
- 		cd.setPixelConversion(conversion);
- 		cd.setRotatedRect(rect);
+		cd.setPixelConversion(conversion);
+		cd.setRotatedRect(rect);
 		cd.setImage(workpiece);
 		cd.calculateExpectedCirclePositions();
 		cd.findCircles();
 		cd.checkCircles();
 		result_circle_detection = cd.isModelCorrect();
 
-		/*
-		Mat circles1 = cd.drawCircles(true);
-		namedWindow("circles", WINDOW_NORMAL);
-		imshow("circles", circles1);
-		*/
 
 		std::vector<cv::Vec3f> circles = cd.getCircles();
-		cout << "Circle detection: " << (result_circle_detection ? "OK" : "FAILED") << endl;
 
 		if (result_circle_detection || check_everything) {
 			//damage detection
 			bool result_damage_detection = !detect_damage(&workpiece, &output_damage, rect, circles, 80, 30, 30, 25);
-			cout << "Damage detection: " << (result_damage_detection ? "OK" : "FAILED") << endl;
 		}
 	}
 
 	if (show) {
-		if (result_quad_detect) {
-			if (!result_circle_detection) {
-				Mat circle_error = cd.drawErrors();
-				imshow(window_name, circle_error);
-				waitKey(0);
-			}
-			else { // true!
-				if (!result_damage_detection) {
-					//namedWindow("damage", WINDOW_NORMAL);
-					imshow(window_name, output_damage);
-					waitKey(0);
-				}
-			}
-		} else {
+		Mat resized_error_output;
+		if (!result_quad_detect) {
 			Mat color_workpiece;
 			cv::cvtColor(workpiece, color_workpiece, CV_GRAY2RGB);
 			draw_quad_info(color_workpiece, &rect, Scalar(0, 0, 255), NULL, Scalar(0, 255, 0));
-			imshow(window_name, color_workpiece);
-			waitKey(0);
+			resize(color_workpiece, resized_error_output, size_image);
+			resized_error_output.copyTo(gui(video));
+
 		}
+		else if (!result_circle_detection) {
+			Mat circle_error = cd.drawErrors();
+			resize(circle_error, resized_error_output, size_image);
+			resized_error_output.copyTo(gui(video));
+		}
+		else if (!result_damage_detection) {
+			resize(output_damage, resized_error_output, size_image);
+			resized_error_output.copyTo(gui(video));
+		}
+		else {
+			overall_result = true;
+		}
+		imshow(window_name, gui);
 	}
 
-	bool result_overall = result_quad_detect && result_circle_detection && result_damage_detection;
-	cout << "Overall result: " << (result_overall ? "OK" : "FAILED") << endl;
 }
 
 double calibrate_px2cm(Mat img, double calibration_length) {
@@ -238,7 +282,7 @@ double calibrate_px2cm(Mat img, double calibration_length) {
 	std::sort(contours.begin(), contours.end(), [](const Pvec& a, const Pvec& b) { return minAreaRect(a).size.area() > minAreaRect(b).size.area(); });
 
 	if (contours.empty()) {
-		cout << "Error: no contours found during calibration process!" << endl;
+		//cout << "Error: no contours found during calibration process!" << endl;
 		return -1.0;
 	}
 
@@ -255,8 +299,6 @@ double calibrate_px2cm(Mat img, double calibration_length) {
 
 	double px2cm = calibration_length / max_line;
 
-	cout << "==========" << endl;
-	cout << "Calibration result: " << px2cm << endl;
 	return px2cm;
 }
 
@@ -310,4 +352,45 @@ void test_routine(string image_folder, string output_path) {
 	}
 	cout << "TEST ROUTINE DONE, RESULT STORED IN: " << output_path << endl;
 	std::cout.rdbuf(coutbuf); //redirect std::cout to origin
+}
+
+void callBackFunc(int event, int x, int y, int flags, void* userdata) {
+	if (event == EVENT_LBUTTONDOWN) {
+		//test image
+		if (test_button.contains(Point(x, y)) && IS_CALIBRATED) {
+			if (test_finished) {
+				test_finished = false;
+				overall_result = false;
+			}
+			else {
+				test_image(image_to_test);
+				test_finished = true;
+			}
+
+		}
+
+		//calibrate
+		else if (calibrate_button.contains(Point(x, y))) {
+			if (test_from_filepath)
+				image_to_test = imread(calibration_image);
+
+			PX2CM = calibrate_px2cm(image_to_test, 2.0);
+			if (PX2CM > 0) {
+				IS_CALIBRATED = true;
+			}
+			else {
+				IS_CALIBRATED = false;
+			}
+		}
+
+
+		//exit
+		else if (exit_button.contains(Point(x, y))) {
+			exit_program = true;
+		}
+	}
+	if (event == EVENT_LBUTTONUP) {
+	}
+
+	imshow(window_name, gui);
 }
